85ebe51a1a98eb79218a8b193928d5859df427b6
Favour Godbless <only1fazzy@sfs-wifi-dhcp-10-143-65-122.wifi.sfsu.edu>
2025-11-12 11:32:10 -0800

    Link hello.js to index.html and add interactive button functionality

---
+++ assignments/4/scripts/hello.js
@@ -0,0 +1,7 @@
+// hello.js
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("greetButton");
+  button.addEventListener("click", function() {
+    alert("Hello! Welcome to my Assignment 4 webpage!");
+  });
+});
